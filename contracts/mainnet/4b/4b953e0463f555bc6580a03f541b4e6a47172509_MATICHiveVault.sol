b58ca85e77f97e6b9476472a2aee18c0c47a5f16
minion <minion>
2022-07-20 05:30:05 +0000

    with 💕

---
+++ contracts/mainnet/4b/4b953e0463f555bc6580a03f541b4e6a47172509_MATICHiveVault.sol
@@ -0,0 +1,43 @@
+// MATIC Hive V2 - 3% daily in MATIC
+// 🌎 Website: https://matic.hiveminer.finance/
+// 📱 Telegram: https://t.me/hivefiv2
+// 🌐 Twitter: https://twitter.com/hivefiv2
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract MATICHiveVault {
+    address hiveAddress;
+
+    modifier onlyHive() {
+		require(msg.sender == hiveAddress, "Only Hive");
+		_;
+	}
+
+
+    constructor(address _hiveAddress) {
+        hiveAddress = _hiveAddress;
+    }
+
+    fallback() external payable {
+        // custom function code
+    }
+
+    receive() external payable {
+        // custom function code
+    }
+
+    function fundHive(uint256 amount) external onlyHive {
+        uint256 balance = address(this).balance;
+        if (balance >= amount) {
+            payable(hiveAddress).transfer(amount);
+        } else if(balance > 0) {
+            payable(hiveAddress).transfer(balance);
+        }
+    }
+
+    function getBalance() public view returns(uint256) {
+        return address(this).balance;
+    }
+}
