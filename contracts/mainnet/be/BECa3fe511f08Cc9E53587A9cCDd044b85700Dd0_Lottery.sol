784996d1d3ba0e5a7ea42285eee1aa1a56358ca2
minion <minion>
2022-07-25 05:18:02 +0000

    with 💕

---
+++ contracts/mainnet/be/BECa3fe511f08Cc9E53587A9cCDd044b85700Dd0_Lottery.sol
@@ -0,0 +1,39 @@
+/**
+ *Submitted for verification at polygonscan.com on 2022-07-25
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+
+contract Lottery {
+
+    // Event definations
+    event LotteryWinner(address[] participants, address winner, string geohash, uint256 nftId);
+
+    function randomNum(
+        uint256 _mod,
+        uint256 _seed,
+        uint256 _salt
+    ) internal view returns (uint256) {
+        uint256 num = uint256(
+            keccak256(
+                abi.encodePacked(block.timestamp, msg.sender, _seed, _salt)
+            )
+        ) % _mod;
+        return num;
+    }
+    // External functions
+    function lottery(address[] memory participantList, uint256 salt, string calldata geohash, uint256 nftId)
+        external
+        returns (address)
+    {
+        uint256 number = randomNum(
+            participantList.length,
+            block.timestamp,
+            participantList.length + salt + 1
+        );
+        emit LotteryWinner(participantList, participantList[number], geohash, nftId);
+        return participantList[number];
+    }
+}
