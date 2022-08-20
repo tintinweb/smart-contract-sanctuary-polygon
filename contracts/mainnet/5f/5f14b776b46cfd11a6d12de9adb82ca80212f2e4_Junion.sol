414873793cd1c425064e6f699089f48de373b8e0
minion <minion>
2022-08-20 11:09:02 +0000

    with 💕

---
+++ contracts/mainnet/5f/5f14b776b46cfd11a6d12de9adb82ca80212f2e4_Junion.sol
@@ -0,0 +1,24 @@
+/**
+ *Submitted for verification at polygonscan.com on 2022-08-20
+*/
+
+/*
+░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░████╗░░░███╗░░██╗░░░
+░░░░░░╚═██║░░░████╗░██║░░░
+░░░░░░░░██║░░░██╔██╗██║░░░
+░░░██╗░░██║░░░██║╚████║░░░
+░░░╚█████║█████╔╝░╚███║░░░
+░░░░╚════╝╚════╝░░░╚══╝░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░
+https://youtube.com/Jun1on
+*/
+pragma solidity 0.8.16;
+contract Junion {
+    function JunionYoutubeXD_clgqmmkfvuba() external{}
+    function JunionYoutubeXD_iyauscmlzbka() external{}
+    function JunionYoutubeXD_fyeyjaposjbk() external{}
+    function JunionYoutubeXD_dashhvetozhe() external{}
+    function SubToJunionOnYoutubeXD_lvfsnfguwqne() external{}
+    function JunionWasHereXD_ufpfzhujazyn() external{}
+}
