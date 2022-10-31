43aa219b653081ad289b90caae48182864c94159
minion <minion>
2022-10-31 21:12:28 +0000

    with 💕

---
+++ contracts/mainnet/77/77b2c1681d5816bc78d076e24100099f5b4e4324_BeatBattle_15.sol
@@ -0,0 +1,125 @@
+/**
+ *Submitted for verification at polygonscan.com on 2022-10-31
+*/
+
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract BeatBattle_15 {
+
+    mapping(address=> uint256) votingTickets;
+
+    uint256 private _counter = 0;
+
+    struct Contestant {
+        bool regestration;
+        address _address;
+        uint256 number;
+        string date;
+        string link;
+        uint256 votes;
+    }
+
+    Contestant[5] public contestants;
+
+    function GetRegistration() public view returns (bool value) {
+        for (uint256 i=0; i<5; i++){
+            if (msg.sender == contestants[i]._address){
+                i=5;
+                return value=true;
+            }        
+        }
+        return value=false;
+    }
+
+    function SetRegistrationTrue(string memory ueDate, string memory ueLink) public {
+        require(_counter < 5, "BeatBattle_15::Exceeds maximum contestants");
+        for (uint256 i=0; i<5; i++){
+            if(contestants[i].regestration == false){
+                contestants[_counter].regestration = true;
+                contestants[_counter]._address = msg.sender;
+                contestants[_counter].number = _counter;
+                contestants[_counter].date = ueDate;
+                contestants[_counter].link = ueLink;
+                _counter++;
+                break;
+            }
+        }
+    }
+
+    function SetRegistrationFalse() public {
+        for (uint256 i = 0; i < 5; i++) {
+            if (contestants[i]._address == msg.sender) {
+                delete contestants[i];
+                _counter--;
+                break;
+            }
+        }
+    }
+
+    function GetCounter() public view returns (uint256) {
+        return _counter;
+    }
+
+
+    function GetContestants() public view returns (
+        address address1, address address2, address address3, address address4, address address5,
+        uint256 votes1, uint256 votes2, uint256 votes3, uint256 votes4, uint256 votes5
+        ) {
+               
+        address1=contestants[0]._address;
+        address2=contestants[1]._address;
+        address3=contestants[2]._address;
+        address4=contestants[3]._address;
+        address5=contestants[4]._address;
+
+        votes1=contestants[0].votes;
+        votes2=contestants[1].votes;
+        votes3=contestants[2].votes;
+        votes4=contestants[3].votes;
+        votes5=contestants[4].votes;
+
+        return (
+            address1, address2, address3, address4, address5,
+            votes1, votes2, votes3, votes4, votes5
+            );
+    }
+
+    function GetDates() public view returns (
+        string memory date1, string memory date2, string memory date3, string memory date4, string memory date5
+        ) {
+
+        date1=contestants[0].date;
+        date2=contestants[1].date;
+        date3=contestants[2].date;
+        date4=contestants[3].date;
+        date5=contestants[4].date;
+
+        return (
+            date1, date2, date3, date4, date5
+            );
+    }
+
+    function Vote (address faddress) public returns (uint votestatus){     
+    votestatus = 0;    
+        if (votingTickets[msg.sender] <2){          
+            for (uint256 i = 0; i < 5; i++) {
+                if (contestants[i]._address == faddress) {
+                    votingTickets[msg.sender]++; 
+                    contestants[i].votes++;
+                    votestatus = 1;
+                    i=5;
+                    return votestatus;
+                }
+            }
+        }
+        votestatus = 2;
+        return votestatus;
+    }
+
+    function GetVotingTickets () public view returns (uint256 fTickets){
+        fTickets=votingTickets[msg.sender];
+        return fTickets;
+    }
+
+}
