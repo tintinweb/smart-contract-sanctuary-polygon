d51ac7f373af1a56c6978322ad9f8c6ad62cb181
minion <minion>
2022-12-20 21:11:14 +0000

    with 💕

---
+++ contracts/mainnet/2b/2bA44748f8fD5d04332a0178B091B4226A8EB9cb_LessonScheduler.sol
@@ -0,0 +1,110 @@
+/**
+ *Submitted for verification at polygonscan.com on 2022-12-20
+*/
+
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;
+
+
+
+contract LessonScheduler
+{
+
+    address constant tAddress = 0x89bb45Bf3576D5C40a67f633fFADaf7CA562b164;
+
+    string[] emails;
+    address[] elegibleAddresses;
+    uint256[] public usedTokensList;
+    mapping(uint256 => address) tokenIDToAddress;
+    address immutable ownerAddress;
+
+    constructor()
+    {
+
+        ownerAddress = msg.sender;
+
+    }
+
+
+
+    function CheckIfTokenIsAvailable(uint256 tokenIndex) public view returns(bool)
+    {
+
+        if(usedTokensList.length == 0)
+        {
+
+            return true;
+            
+
+        }
+        else
+        {
+
+            uint256 i = 0;
+
+            for(i; i < usedTokensList.length; i++)
+            {
+
+                if(tokenIndex == usedTokensList[i])
+                {
+
+                    return false;
+
+
+                }
+
+            }
+
+            if (i <= usedTokensList.length)
+            {
+
+                return true;
+
+            }
+
+        }
+
+        return false;
+    }
+
+
+    function ClaimFreeConsultation(string memory inputEMail, uint256 tokenID) external returns (bool)
+    {
+
+        (, bytes memory data) = tAddress.call(
+
+            abi.encodeWithSignature("ownerOf(uint256)", tokenID)
+
+        );
+
+
+        address decodedAddress = abi.decode(data, (address));
+        require(CheckIfTokenIsAvailable(tokenID) == true, "This token has already been used to claim free consultation");
+
+        require(decodedAddress == msg.sender, "Sender doesn't match token owner");
+
+
+        emails.push(inputEMail);
+        usedTokensList.push(tokenID);
+
+        return true;
+
+    }
+
+    function RetrieveEMails() public isDev returns(string[] memory)
+    {
+
+        return emails;
+
+    }
+
+    modifier isDev
+    {
+
+        require(msg.sender == ownerAddress, "Not owner");
+        _;
+
+    }
+
+}
---
+++ contracts/mainnet/5b/5b40536b398051ea8dbaf4d7bb6ace09896153a0_LessonScheduler.sol
@@ -0,0 +1,186 @@
+/**
+ *Submitted for verification at polygonscan.com on 2022-12-20
+*/
+
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;
+
+
+
+contract LessonScheduler
+{
+
+    address constant tAddress = 0x89bb45Bf3576D5C40a67f633fFADaf7CA562b164;
+    address constant storageAddress = 0x1e48764196B8aF0c35175C9dD996785F8dBb0858;
+    //address constant storageAddress = 0xd9145CCE52D386f254917e481eB44e9943F39138;
+
+    struct Request{
+        string eMail;
+        uint256 tokenID;
+        address userAddress;
+        bool approved;
+    }
+
+    address public addressThatReturned;
+    bool public addrressIsElegible = true;
+    uint256 public retrievedNumber = 0;
+    uint256 numOfRequests = 0;
+    Request[] requests;
+    string[] emails;
+    address[] elegibleAddresses;
+    uint256[] public usedTokensList;
+    mapping(uint256 => address) tokenIDToAddress;
+    address immutable ownerAddress;
+
+    constructor()
+    {
+
+        ownerAddress = msg.sender;
+        elegibleAddresses.push(0xa8c7d5818A255A1856b31177E5c96E1D61c83991);
+
+    }
+
+
+    function TestStorageCall() public returns (bool, uint256)
+    {
+
+        (bool success, bytes memory data) = storageAddress.call
+        (
+
+            abi.encodeWithSignature("RetrieveNumber()")
+
+        );
+
+        uint256 decodedData = abi.decode(data, (uint256));
+        retrievedNumber = decodedData;
+
+        return (success, decodedData);
+        
+    }
+
+
+    function RetrieveRequesters() public view returns(address[] memory)
+    {
+
+        return elegibleAddresses;
+
+    }
+
+    function CheckIfTokenIsAvailable(uint256 tokenIndex) public view returns(bool)
+    {
+
+        if(usedTokensList.length == 0)
+        {
+
+            return true;
+            
+
+        }
+        else
+        {
+
+            uint256 i = 0;
+
+            for(i; i < usedTokensList.length; i++)
+            {
+
+                if(tokenIndex == usedTokensList[i])
+                {
+
+                    return false;
+
+
+                }
+
+            }
+
+            if (i <= usedTokensList.length)
+            {
+
+                return true;
+
+            }
+
+        }
+
+        return false;
+    }
+
+    function CreateRequest(string memory inputEMail, address userAddres, uint256 tokenID) internal
+    {
+
+       requests.push(Request({eMail: inputEMail, tokenID: tokenID, userAddress: userAddres, approved: false}));
+       elegibleAddresses.push(userAddres);
+       numOfRequests++;
+
+    }
+
+    function GetNumberOfRequests() public view isDev returns(uint256)
+    {
+
+        return numOfRequests;
+
+    }
+
+    function GetRequestInfo(uint256 requestID) public view isDev returns(Request memory)
+    {
+
+        return requests[requestID];
+
+    }
+
+    function ApproveRequest(uint256 requestID) public isDev
+    {
+
+        Request memory tempRequest = requests[requestID];
+        tempRequest.approved = true;
+        emails.push(tempRequest.eMail);
+
+    }
+
+    function ClaimFreeConsultation(string memory inputEMail, uint256 tokenID) external returns (address)
+    {
+
+        (bool success, bytes memory data) = tAddress.call(
+
+            abi.encodeWithSignature("ownerOf(uint256)", tokenID)
+
+        );
+
+
+            address decodedAddress = abi.decode(data, (address));
+            addressThatReturned = decodedAddress;
+            
+            if(decodedAddress == msg.sender)
+            {
+
+                addrressIsElegible = true;
+
+            }
+            else
+            {
+
+                addrressIsElegible = false;
+
+            }
+            return decodedAddress;
+        //CreateRequest(inputEMail, msg.sender, tokenID);
+    }
+
+    function RetrieveEMails() public view isDev returns(string[] memory)
+    {
+
+        return emails;
+
+    }
+
+    modifier isDev
+    {
+
+        require(msg.sender == ownerAddress, "Not owner");
+        _;
+
+    }
+
+}
