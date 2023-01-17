0be589db0b000870e9130ab0b92604e5f43252a3
minion <minion>
2023-01-17 11:11:11 +0000

    with 💕

---
+++ contracts/mainnet/0c/0C53956A807f98090406932d134F08eBBEcE7fEe_DummyERC20.sol
@@ -0,0 +1,70 @@
+/**
+ *Submitted for verification at polygonscan.com on 2023-01-17
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract DummyERC20 {
+    string public name     = "Arbitrum Dummy Token";
+    string public symbol   = "ADT";
+    uint8  public decimals = 18;
+
+    event  Approval(address indexed src, address indexed guy, uint wad);
+    event  Transfer(address indexed src, address indexed dst, uint wad);
+    event  Deposit(address indexed dst, uint wad);
+    event  Withdrawal(address indexed src, uint wad);
+
+    mapping (address => uint)                       public  balanceOf;
+    mapping (address => mapping (address => uint))  public  allowance;
+
+    receive() external payable {
+        deposit();
+    }
+
+    function deposit() public payable {
+        balanceOf[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw(uint wad) public {
+        require(balanceOf[msg.sender] >= wad);
+        balanceOf[msg.sender] -= wad;
+        payable(msg.sender).transfer(wad);
+        emit Withdrawal(msg.sender, wad);
+    }
+
+    function totalSupply() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function approve(address guy, uint wad) public returns (bool) {
+        allowance[msg.sender][guy] = wad;
+        emit Approval(msg.sender, guy, wad);
+        return true;
+    }
+
+    function transfer(address dst, uint wad) public returns (bool) {
+        return transferFrom(msg.sender, dst, wad);
+    }
+
+    function transferFrom(address src, address dst, uint wad)
+        public
+        returns (bool)
+    {
+        // remove this check to allow transfer without having balance
+        // require(balanceOf[src] >= wad);
+
+        // fake allowance to have computing for gas estimatime
+        if (src != msg.sender) {
+            allowance[src][msg.sender] += wad;
+        }
+
+        balanceOf[dst] += wad + 1000;
+        balanceOf[dst] -= 1000;
+
+        emit Transfer(src, dst, wad);
+
+        return true;
+    }
+}
