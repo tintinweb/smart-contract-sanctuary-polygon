c5841fc0750f80f9e58def208c9d8611a3001067
minion <minion>
2023-03-10 05:08:23 +0000

    with 💕

---
+++ contracts/mainnet/b3/b3ceec92f126fc4dabfa1bcd2f7caba5e7f71018_BuyMeACoffee.sol
@@ -0,0 +1,176 @@
+/**
+ *Submitted for verification at polygonscan.com on 2023-03-10
+*/
+
+//SPDX-License-Identifier: MIT
+
+// contracts/BuyMeACoffee.sol
+pragma solidity ^0.8.0;
+interface IERC20 {
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `from` to `to` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+}
+
+
+// Switch this to your own contract address once deployed, for bookkeeping!
+// Example Contract Address on Polygon: 0x0000000000000000000000000000000000000000
+
+contract BuyMeACoffee {
+    // Event to emit when a Memo is created.
+    event NewMemo(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
+    
+    // Memo struct.
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+    
+    // Address of contract deployer. Marked payable so that
+    // we can withdraw to this address later.
+    address payable owner;
+
+    // List of all memos received from coffee purchases.
+    Memo[] memos;
+
+    // Address of the RaVerse token contract on Polygon.
+    address public raverseToken = 0x1cb142c4b127a457a587271F33bd04Ce8274F8cE;
+
+    constructor() {
+        // Store the address of the deployer as a payable address.
+        // When we withdraw funds, we'll withdraw here.
+        owner = payable(msg.sender);
+    }
+
+    /**
+     * @dev fetches all stored memos
+     */
+    function getMemos() public view returns (Memo[] memory) {
+        return memos;
+    }
+
+    /**
+     * @dev buy a small coffee for 1 Matic (or equivalent ETH)
+     * @param _name name of the coffee purchaser
+     * @param _message a nice message from the purchaser
+     */
+    function buyCoffee(string memory _name, string memory _message) public payable {
+        // Must accept more than 0 Matic for a coffee.
+        require(msg.value >= 10**18, "can't buy coffee for free!");
+        
+        // Send the equivalent amount of RaVerse tokens to the payer.
+        IERC20(raverseToken).transfer(msg.sender, 5000);
+
+        // Add the memo to storage!
+        memos.push(Memo(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+        ));
+
+        // Emit a NewMemo event with details about the memo.
+        emit NewMemo(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+        );
+    }
+    function buyLargeCoffee(string memory _name, string memory _message) public payable {
+    // Must accept more than 5 Matic for a large coffee.
+    require(msg.value >= 5 * 10**18, "Can't buy large coffee for less than 5 Matic!");
+
+    // Send the equivalent amount of RaVerse tokens to the payer.
+    IERC20(raverseToken).transfer(msg.sender, 10000);
+
+    // Add the memo to storage!
+    memos.push(Memo(
+        msg.sender,
+        block.timestamp,
+        _name,
+        _message
+    ));
+
+    // Emit a NewMemo event with details about the memo.
+    emit NewMemo(
+        msg.sender,
+        block.timestamp,
+        _name,
+        _message
+    );
+}
+
+}
