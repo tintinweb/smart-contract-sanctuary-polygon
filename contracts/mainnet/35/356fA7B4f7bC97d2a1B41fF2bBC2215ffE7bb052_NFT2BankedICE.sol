670419f08e50a564781a5e308ff2323d2af1e16e
minion <minion>
2023-06-09 05:09:10 +0000

    with 💕

---
+++ contracts/mainnet/35/356fA7B4f7bC97d2a1B41fF2bBC2215ffE7bb052_NFT2BankedICE.sol
@@ -0,0 +1,516 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.9.0) (access/Ownable.sol)
+
+pragma solidity ^0.8.0;
+
+import "../utils/Context.sol";
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
+    function _checkOwner() internal view virtual {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby disabling any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.9.0) (token/ERC721/IERC721.sol)
+
+pragma solidity ^0.8.0;
+
+import "../../utils/introspection/IERC165.sol";
+
+/**
+ * @dev Required interface of an ERC721 compliant contract.
+ */
+interface IERC721 is IERC165 {
+    /**
+     * @dev Emitted when `tokenId` token is transferred from `from` to `to`.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
+    /**
+     * @dev Emitted when `owner` enables `approved` to manage the `tokenId` token.
+     */
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+
+    /**
+     * @dev Emitted when `owner` enables or disables (`approved`) `operator` to manage all of its assets.
+     */
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+    /**
+     * @dev Returns the number of tokens in ``owner``'s account.
+     */
+    function balanceOf(address owner) external view returns (uint256 balance);
+
+    /**
+     * @dev Returns the owner of the `tokenId` token.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function ownerOf(uint256 tokenId) external view returns (address owner);
+
+    /**
+     * @dev Safely transfers `tokenId` token from `from` to `to`.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must exist and be owned by `from`.
+     * - If the caller is not `from`, it must be approved to move this token by either {approve} or {setApprovalForAll}.
+     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
+     *
+     * Emits a {Transfer} event.
+     */
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes calldata data) external;
+
+    /**
+     * @dev Safely transfers `tokenId` token from `from` to `to`, checking first that contract recipients
+     * are aware of the ERC721 protocol to prevent tokens from being forever locked.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must exist and be owned by `from`.
+     * - If the caller is not `from`, it must have been allowed to move this token by either {approve} or {setApprovalForAll}.
+     * - If `to` refers to a smart contract, it must implement {IERC721Receiver-onERC721Received}, which is called upon a safe transfer.
+     *
+     * Emits a {Transfer} event.
+     */
+    function safeTransferFrom(address from, address to, uint256 tokenId) external;
+
+    /**
+     * @dev Transfers `tokenId` token from `from` to `to`.
+     *
+     * WARNING: Note that the caller is responsible to confirm that the recipient is capable of receiving ERC721
+     * or else they may be permanently lost. Usage of {safeTransferFrom} prevents loss, though the caller must
+     * understand this adds an external call which potentially creates a reentrancy vulnerability.
+     *
+     * Requirements:
+     *
+     * - `from` cannot be the zero address.
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must be owned by `from`.
+     * - If the caller is not `from`, it must be approved to move this token by either {approve} or {setApprovalForAll}.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address from, address to, uint256 tokenId) external;
+
+    /**
+     * @dev Gives permission to `to` to transfer `tokenId` token to another account.
+     * The approval is cleared when the token is transferred.
+     *
+     * Only a single account can be approved at a time, so approving the zero address clears previous approvals.
+     *
+     * Requirements:
+     *
+     * - The caller must own the token or be an approved operator.
+     * - `tokenId` must exist.
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address to, uint256 tokenId) external;
+
+    /**
+     * @dev Approve or remove `operator` as an operator for the caller.
+     * Operators can call {transferFrom} or {safeTransferFrom} for any token owned by the caller.
+     *
+     * Requirements:
+     *
+     * - The `operator` cannot be the caller.
+     *
+     * Emits an {ApprovalForAll} event.
+     */
+    function setApprovalForAll(address operator, bool approved) external;
+
+    /**
+     * @dev Returns the account approved for `tokenId` token.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function getApproved(uint256 tokenId) external view returns (address operator);
+
+    /**
+     * @dev Returns if the `operator` is allowed to manage all of the assets of `owner`.
+     *
+     * See {setApprovalForAll}
+     */
+    function isApprovedForAll(address owner, address operator) external view returns (bool);
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (utils/introspection/IERC165.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Interface of the ERC165 standard, as defined in the
+ * https://eips.ethereum.org/EIPS/eip-165[EIP].
+ *
+ * Implementers can declare support of contract interfaces, which can then be
+ * queried by others ({ERC165Checker}).
+ *
+ * For an implementation, see {ERC165}.
+ */
+interface IERC165 {
+    /**
+     * @dev Returns true if this contract implements the interface defined by
+     * `interfaceId`. See the corresponding
+     * https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified[EIP section]
+     * to learn more about how these ids are created.
+     *
+     * This function call must use less than 30 000 gas.
+     */
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.7;
+
+contract EIP712Base {
+
+    struct EIP712Domain {
+        string name;
+        string version;
+        uint256 chainId;
+        address verifyingContract;
+    }
+
+    bytes32 internal constant EIP712_DOMAIN_TYPEHASH = keccak256(bytes("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"));
+    bytes32 internal domainSeperator;
+
+    constructor(string memory name, string memory version) {
+        domainSeperator = keccak256(abi.encode(
+			EIP712_DOMAIN_TYPEHASH,
+			keccak256(bytes(name)),
+			keccak256(bytes(version)),
+			getChainID(),
+			address(this)
+		));
+    }
+
+    function getChainID() internal pure returns (uint256 id) {
+		assembly {
+			id := 1 // set to Goerli for now, Mainnet later
+		}
+	}
+
+    function getDomainSeperator() private view returns(bytes32) {
+		return domainSeperator;
+	}
+
+    /**
+    * Accept message hash and returns hash message in EIP712 compatible form
+    * So that it can be used to recover signer from signature signed using EIP712 formatted data
+    * https://eips.ethereum.org/EIPS/eip-712
+    * "\\x19" makes the encoding deterministic
+    * "\\x01" is the version byte to make it compatible to EIP-191
+    */
+    function toTypedMessageHash(bytes32 messageHash) internal view returns(bytes32) {
+        return keccak256(abi.encodePacked("\x19\x01", getDomainSeperator(), messageHash));
+    }
+}
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.7;
+
+import "./EIP712Base.sol";
+
+abstract contract EIP712MetaTransaction is EIP712Base {
+
+    bytes32 private constant META_TRANSACTION_TYPEHASH =
+        keccak256(
+            bytes(
+                "MetaTransaction(uint256 nonce,address from,bytes functionSignature)"
+            )
+        );
+
+    event MetaTransactionExecuted(
+        address userAddress,
+        address payable relayerAddress,
+        bytes functionSignature
+    );
+
+    mapping(address => uint256) internal nonces;
+
+    /*
+     * Meta transaction structure.
+     * No point of including value field here as if user is doing value transfer then he has the funds to pay for gas
+     * He should call the desired function directly in that case.
+     */
+    struct MetaTransaction {
+		uint256 nonce;
+		address from;
+        bytes functionSignature;
+	}
+
+    function executeMetaTransaction(
+        address userAddress,
+        bytes memory functionSignature,
+        bytes32 sigR,
+        bytes32 sigS,
+        uint8 sigV
+    )
+        public
+        payable
+        returns(bytes memory)
+    {
+        MetaTransaction memory metaTx = MetaTransaction(
+            {
+                nonce: nonces[userAddress],
+                from: userAddress,
+                functionSignature: functionSignature
+            }
+        );
+
+        require(
+            verify(
+                userAddress,
+                metaTx,
+                sigR,
+                sigS,
+                sigV
+            ), "Signer and signature do not match"
+        );
+
+	    nonces[userAddress] =
+	    nonces[userAddress] + 1;
+
+        // Append userAddress at the end to extract it from calling context
+        (bool success, bytes memory returnData) = address(this).call(
+            abi.encodePacked(
+                functionSignature,
+                userAddress
+            )
+        );
+
+        require(
+            success,
+            'Function call not successful'
+        );
+
+        emit MetaTransactionExecuted(
+            userAddress,
+            payable(msg.sender),
+            functionSignature
+        );
+
+        return returnData;
+    }
+
+    function hashMetaTransaction(
+        MetaTransaction memory metaTx
+    )
+        internal
+        pure
+        returns (bytes32)
+    {
+		return keccak256(
+		    abi.encode(
+                META_TRANSACTION_TYPEHASH,
+                metaTx.nonce,
+                metaTx.from,
+                keccak256(metaTx.functionSignature)
+            )
+        );
+	}
+
+    function verify(
+        address user,
+        MetaTransaction memory metaTx,
+        bytes32 sigR,
+        bytes32 sigS,
+        uint8 sigV
+    )
+        internal
+        view
+        returns (bool)
+    {
+        address signer = ecrecover(
+            toTypedMessageHash(
+                hashMetaTransaction(metaTx)
+            ),
+            sigV,
+            sigR,
+            sigS
+        );
+
+        require(
+            signer != address(0x0),
+            'Invalid signature'
+        );
+		return signer == user;
+	}
+
+    function msgSender() internal view returns(address sender) {
+        if(msg.sender == address(this)) {
+            bytes memory array = msg.data;
+            uint256 index = msg.data.length;
+            assembly {
+                // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
+                sender := and(mload(add(array, index)), 0xffffffffffffffffffffffffffffffffffffffff)
+            }
+        } else {
+            sender = msg.sender;
+        }
+        return sender;
+    }
+
+    function getNonce(
+        address _user
+    )
+        external
+        view
+        returns(uint256 nonce)
+    {
+        nonce = nonces[_user];
+    }
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./EIP712MetaTransaction.sol";
+
+contract NFT2BankedICE is Ownable, EIP712MetaTransaction {
+    address public beneficiary;
+    mapping(address => bool) public whitelistedNFTs;
+
+    event NftTransferred(
+        address indexed from,
+        address indexed to,
+        address indexed nftAddress,
+        uint256 tokenId
+    );
+
+    constructor() EIP712Base("NFT2BankedICE", "v1.0") {
+        beneficiary = 0xEA5Fed1D0141F14DE11249577921b08783d6A360; // Set the beneficiary address here
+    }
+
+    function changeBeneficiary(address _newBeneficiary) external onlyOwner {
+        beneficiary = _newBeneficiary;
+    }
+
+    function addWhitelistedNFT(address _nftAddress) external onlyOwner {
+        whitelistedNFTs[_nftAddress] = true;
+    }
+
+    function removeWhitelistedNFT(address _nftAddress) external onlyOwner {
+        whitelistedNFTs[_nftAddress] = false;
+    }
+
+    function transferNFT(address _nftAddress, uint256 _tokenId) external {
+        require(whitelistedNFTs[_nftAddress], "NFT is not whitelisted");
+        IERC721 nftContract = IERC721(_nftAddress);
+        nftContract.safeTransferFrom(msg.sender, beneficiary, _tokenId);
+        emit NftTransferred(msg.sender, beneficiary, _nftAddress, _tokenId);
+    }
+}
+
+//npx hardhat verify --network polygon 0x356fA7B4f7bC97d2a1B41fF2bBC2215ffE7bb052
---
+++ contracts/mainnet/38/3872e75405536194f3f10003dc2df206422de258_PLGReferralRewardV3.sol
@@ -0,0 +1,162 @@
+/**
+ *Submitted for verification at polygonscan.com on 2023-06-09
+*/
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+interface IPLGFactory {
+    function getAddr(string memory key) external view returns (address);
+}
+
+interface IPLGUser {
+    function getUserReferralMap(address account) external view returns (address[] memory);
+    function getUserUpline(address account,uint256 level) external view returns (address[] memory);
+}
+
+interface IPLGv2 {
+    function invest(address account) external view returns (uint256,uint256,uint256,uint256,uint256,uint256,uint256);
+}
+
+interface IWPLG {
+    function depositFor(address recipient) external payable returns (bool);
+}
+
+contract permission {
+    mapping(address => mapping(string => bytes32)) private permit;
+
+    function newpermit(address adr,string memory str) internal { permit[adr][str] = bytes32(keccak256(abi.encode(adr,str))); }
+
+    function clearpermit(address adr,string memory str) internal { permit[adr][str] = bytes32(keccak256(abi.encode("null"))); }
+
+    function checkpermit(address adr,string memory str) public view returns (bool) {
+        if(permit[adr][str]==bytes32(keccak256(abi.encode(adr,str)))){ return true; }else{ return false; }
+    }
+
+    modifier forRole(string memory str) {
+        require(checkpermit(msg.sender,str),"Permit Revert!");
+        _;
+    }
+}
+
+contract PLGReferralRewardV3 is permission {
+    
+    address public owner;
+
+    IPLGFactory factory;
+
+    mapping(address => uint256) public vipBlock;
+    mapping(address => mapping(address => bool)) public isCount;
+    mapping(address => mapping(uint256 => uint256)) public members;
+    mapping(address => mapping(string => mapping(uint256 => uint256))) public recordData;
+
+    constructor(address _factory) {
+        newpermit(msg.sender,"owner");
+        owner = msg.sender;
+        factory = IPLGFactory(_factory);
+    }
+
+    function rewardDistribute(address account,address userContract,string memory dataSlot) public payable forRole("permit") returns (bool) {
+        address[] memory addrs = IPLGUser(userContract).getUserUpline(account,30);
+        for(uint256 i=0; i<30; i++){
+            if(!isCount[account][safeAddr(addrs[i])]){
+                isCount[account][safeAddr(addrs[i])] = true;
+                members[safeAddr(addrs[i])][i+1] += 1;
+            }
+            bool shouldreward = false;
+            uint256 capreward = 0;
+            (uint256 balance,uint256 repeat,,,,uint256 cycle,) = IPLGv2(factory.getAddr("plg_router")).invest(safeAddr(addrs[i]));
+            if(vipBlock[account]>block.timestamp){ shouldreward = true; }else{
+                if(recordData[safeAddr(addrs[i])]["stack"][cycle]+(msg.value/30)>balance * repeat){
+                    if(recordData[safeAddr(addrs[i])]["stack"][cycle]<balance * repeat){
+                        capreward = (balance * repeat) - recordData[safeAddr(addrs[i])]["stack"][cycle];
+                        shouldreward = true;
+                    }
+                }else{ shouldreward = true; }
+            }
+            if(shouldreward){
+                address[] memory refmap = IPLGUser(userContract).getUserReferralMap(safeAddr(addrs[i]));
+                if(vipBlock[safeAddr(addrs[i])]>block.timestamp || refmap.length>i){
+                    uint256 spenderAmount;
+                    if(capreward>0){ spenderAmount = capreward; }else{ spenderAmount = msg.value / 30; }
+                    recordData[safeAddr(addrs[i])][dataSlot][i] += spenderAmount;
+                    recordData[safeAddr(addrs[i])]["stack"][cycle] += spenderAmount;
+                    IWPLG(factory.getAddr("plg_wrapped")).depositFor{ value: spenderAmount }(safeAddr(addrs[i]));
+                }
+            }
+        }
+        _clearStuckBalance(factory.getAddr("plg_pool"));
+        return true;
+    }
+
+    function viewMembersData(address account) public view returns (uint256[] memory,uint256) {
+        uint256[] memory result = new uint256[](31);
+        uint256 sum;
+        for(uint256 i=0; i<30; i++){
+            result[i] = members[account][i];
+            sum += result[i];
+        }
+        return (result,sum);
+    }
+    
+    function updateVIPBlockWithPermit(address account,uint256 timer) public forRole("permit") returns (bool) {
+        _updateVIPBlock(account,timer);
+        return true;
+    }
+
+    function _updateVIPBlock(address account,uint256 timer) internal { vipBlock[account] = timer; }
+
+    function updateRecordData(address account,string memory dataSlot,uint256 index,uint256 amount) public forRole("permit") returns (bool) {
+        recordData[account][dataSlot][index] = amount;
+        return true;
+    }
+
+    function increaseRecordData(address account,string memory dataSlot,uint256 index,uint256 amount) public forRole("permit") returns (bool) {
+        recordData[account][dataSlot][index] += amount;
+        return true;
+    }
+
+    function decreaseRecordData(address account,string memory dataSlot,uint256 index,uint256 amount) public forRole("permit") returns (bool) {
+        recordData[account][dataSlot][index] -= amount;
+        return true;
+    }
+
+    function safeAddr(address account) internal view returns (address) {
+        if(account==address(0)){ return owner; }else{ return account; }
+    }
+
+    function factoryAddressSetting(address _factory) public forRole("owner") returns (bool) {
+        factory = IPLGFactory(_factory);
+        return true;
+    }
+
+    function purgeETH() public forRole("owner") returns (bool) {
+      _clearStuckBalance(owner);
+      return true;
+    }
+
+    function _clearStuckBalance(address receiver) internal {
+      (bool success,) = receiver.call{ value: address(this).balance }("");
+      require(success, "!fail to send eth");
+    }
+
+    function grantRole(address adr,string memory role) public forRole("owner") returns (bool) {
+        newpermit(adr,role);
+        return true;
+    }
+
+    function revokeRole(address adr,string memory role) public forRole("owner") returns (bool) {
+        clearpermit(adr,role);
+        return true;
+    }
+
+    function transferOwnership(address adr) public forRole("owner") returns (bool) {
+        newpermit(adr,"owner");
+        clearpermit(msg.sender,"owner");
+        owner = adr;
+        return true;
+    }
+
+    receive() external payable {}
+}
