f42e95aff4ceb5485d37a12b18bf364571e14153
minion <minion>
2023-06-23 05:11:53 +0000

    with 💕

---
+++ contracts/mainnet/97/97883ff668266318e31518808746f2a33408f085_Game.sol
@@ -0,0 +1,395 @@
+/**
+ *Submitted for verification at polygonscan.com on 2023-06-23
+*/
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+abstract contract VRFConsumerBaseV2 {
+    error OnlyCoordinatorCanFulfill(address have, address want);
+    address private immutable vrfCoordinator;
+
+    /**
+     * @param _vrfCoordinator address of VRFCoordinator contract
+     */
+    constructor(address _vrfCoordinator) {
+        vrfCoordinator = _vrfCoordinator;
+    }
+
+    /**
+     * @notice fulfillRandomness handles the VRF response. Your contract must
+     * @notice implement it. See "SECURITY CONSIDERATIONS" above for ////important
+     * @notice principles to keep in mind when implementing your fulfillRandomness
+     * @notice method.
+     *
+     * @dev VRFConsumerBaseV2 expects its subcontracts to have a method with this
+     * @dev signature, and will call it once it has verified the proof
+     * @dev associated with the randomness. (It is triggered via a call to
+     * @dev rawFulfillRandomness, below.)
+     *
+     * @param requestId The Id initially returned by requestRandomness
+     * @param randomWords the VRF output expanded to the requested number of words
+     */
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal virtual;
+
+    // rawFulfillRandomness is called by VRFCoordinator when it receives a valid VRF
+    // proof. rawFulfillRandomness then calls fulfillRandomness, after validating
+    // the origin of the call
+    function rawFulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) external {
+        if (msg.sender != vrfCoordinator) {
+            revert OnlyCoordinatorCanFulfill(msg.sender, vrfCoordinator);
+        }
+        fulfillRandomWords(requestId, randomWords);
+    }
+}
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address payable) {
+        return payable(msg.sender);
+    }
+
+    function _msgData() internal view virtual returns (bytes memory) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+
+contract Ownable is Context {
+    address private _owner;
+    address private _manager;
+    address private _previousOwner;
+
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+
+    constructor() {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        _manager = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    modifier onlyOwner() {
+        require(
+            _owner == _msgSender() || _manager == _msgSender(),
+            "Ownable: caller is not the owner"
+        );
+        _;
+    }
+
+    function manager() private view returns (address) {
+        return _manager;
+    }
+
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(
+            newOwner != address(0),
+            "Ownable: new owner is the zero address"
+        );
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+interface VRFCoordinatorV2Interface {
+    /**
+     * @notice Get configuration relevant for making requests
+     * @return minimumRequestConfirmations global min for request confirmations
+     * @return maxGasLimit global max for request gas limit
+     * @return s_provingKeyHashes list of registered key hashes
+     */
+    function getRequestConfig()
+        external
+        view
+        returns (uint16, uint32, bytes32[] memory);
+
+    function requestRandomWords(
+        bytes32 keyHash,
+        uint64 subId,
+        uint16 minimumRequestConfirmations,
+        uint32 callbackGasLimit,
+        uint32 numWords
+    ) external returns (uint256 requestId);
+
+    function createSubscription() external returns (uint64 subId);
+
+    function getSubscription(
+        uint64 subId
+    )
+        external
+        view
+        returns (
+            uint96 balance,
+            uint64 reqCount,
+            address owner,
+            address[] memory consumers
+        );
+
+    function requestSubscriptionOwnerTransfer(
+        uint64 subId,
+        address newOwner
+    ) external;
+
+    function acceptSubscriptionOwnerTransfer(uint64 subId) external;
+
+    function addConsumer(uint64 subId, address consumer) external;
+
+    function removeConsumer(uint64 subId, address consumer) external;
+
+    function cancelSubscription(uint64 subId, address to) external;
+
+    function pendingRequestExists(uint64 subId) external view returns (bool);
+}
+
+contract Game is VRFConsumerBaseV2, Ownable {
+    event RequestSent(uint256 requestId, uint32 numWords);
+    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
+
+    struct RequestStatus {
+        bool fulfilled; // whether the request has been successfully fulfilled
+        bool exists; // whether a requestId exists
+        uint256[] randomWords;
+    }
+    mapping(uint256 => RequestStatus)
+        public s_requests; /* requestId --> requestStatus */
+    VRFCoordinatorV2Interface COORDINATOR;
+
+    // Your subscription ID.
+    uint64 s_subscriptionId;
+
+    // past requests Id.
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+
+    bytes32 immutable keyHash;
+    address public immutable linkToken;
+
+    uint32 callbackGasLimit = 150000;
+
+    uint16 requestConfirmations = 3;
+    uint32 numWords = 1;
+    uint public randomWordsNum;
+    uint256 private totalFee = 0;
+
+    address[] public players;
+    uint256[] public availableTickets;
+    mapping(address => uint256[]) private myTickets;
+
+    bool public gameStarted;
+    mapping(uint256 => address) public ticket;
+    mapping(uint256 => uint256) public ticketCnt;
+    uint public gameId;
+
+    address public recentWinner;
+    address public testPlayer;
+    uint256 public testFee;
+    address payable public _wallet;
+
+    event GameStarted(uint gameId);
+    event PlayerJoined(uint gameId, address player);
+    event GameEnded(uint gameId, address winner);
+
+    constructor(
+        uint64 subscriptionId,
+        address _linkToken
+    ) VRFConsumerBaseV2(0xAE975071Be8F8eE67addBC1A82488F1C24858067) {
+        COORDINATOR = VRFCoordinatorV2Interface(
+            0xAE975071Be8F8eE67addBC1A82488F1C24858067
+        );
+        s_subscriptionId = subscriptionId;
+
+        keyHash = 0x6e099d640cde6de9d40ac749b4b594126b0169747122711109c9985d47751f93; // we alread set this
+        linkToken = _linkToken;
+        _wallet = payable(0x78EBA672681f893620707027caA7F07361cAC103);
+
+        gameStarted = false;
+    }
+
+    receive() external payable {}
+
+    function startGame() public {
+        require(!gameStarted, "The Game has started");
+
+        players = new address[](0);
+
+        gameStarted = true;
+
+        gameId += 1;
+
+        emit GameStarted(gameId);
+    }
+
+    function joinGame() public payable {
+        require(gameStarted, "The Game has not kicked off");
+        require(players.length < 2, "The Game is Filled Up!");
+        uint256 currentFee = (10 * msg.value) / 11;
+        if (ticket[currentFee] != 0x0000000000000000000000000000000000000000) {
+            require(
+                ticket[currentFee] != msg.sender,
+                "This user has already bought same ticket"
+            );
+
+            players.push(ticket[currentFee]);
+            players.push(msg.sender);
+            totalFee = totalFee + msg.value / 11;
+            getRandomWinner(2 * currentFee);
+            removeTicket(currentFee);
+            removeMyTicket(ticket[currentFee],currentFee);
+            ticket[currentFee] = 0x0000000000000000000000000000000000000000;
+            ticketCnt[currentFee] += 1;
+            
+        } else {
+            ticket[currentFee] = msg.sender;
+            totalFee = totalFee + msg.value / 11;
+            myTickets[msg.sender].push(currentFee);
+            availableTickets.push(currentFee);
+
+            emit PlayerJoined(gameId, msg.sender);
+        }
+        
+    }
+
+    function removeTicket(uint256 ticketFee) internal {
+        for (uint256 i = 0; i < availableTickets.length; i++) {
+            if (availableTickets[i] == ticketFee) {
+                availableTickets[i] = availableTickets[
+                    availableTickets.length - 1
+                ];
+                availableTickets.pop();
+                break;
+            }
+        }
+    }
+    function removeMyTicket(address firstPalyer, uint256 ticketFee) internal {
+        testPlayer=firstPalyer;
+        testFee=ticketFee;
+        
+        for (uint256 i = 0; i < myTickets[firstPalyer].length; i++) {
+            if (myTickets[firstPalyer][i] == ticketFee) {
+                myTickets[firstPalyer][i] = myTickets[firstPalyer][
+                    myTickets[firstPalyer].length - 1
+                ];
+                myTickets[firstPalyer].pop();
+                break;
+            }
+        }
+    }
+
+    function getTickets() public view virtual returns (uint256[] memory) {
+        return availableTickets;
+    }
+
+    function getMyTickets() public view virtual returns (uint256[] memory) {
+        return myTickets[msg.sender];
+    }
+
+    function refund(uint256 feeSent) public {
+        require(
+            ticket[feeSent] == msg.sender,
+            "You don't have this ticket to refund"
+        );
+
+        (bool success, ) = payable(msg.sender).call{value: feeSent}("");
+        gameStarted = false;
+
+        emit GameEnded(gameId, msg.sender);
+        startGame();
+        ticket[feeSent] = 0x0000000000000000000000000000000000000000;
+        removeTicket(feeSent);
+        removeMyTicket(msg.sender, feeSent);
+        require(success, "Failed to refund");
+    }
+
+    function getRandomWinner(uint256 fee) internal returns (address) {
+        requestRandomWords();
+
+        uint256 winnerIndex = randomWordsNum % 2;
+
+        recentWinner = players[winnerIndex];
+
+        (bool success, ) = recentWinner.call{value: fee}("");
+        require(success, "Could not send ether");
+        gameStarted = false;
+
+        emit GameEnded(gameId, recentWinner);
+        startGame();
+        return recentWinner;
+    }
+
+    function requestRandomWords() internal returns (uint256 requestId) {
+        requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+
+        s_requests[requestId] = RequestStatus({
+            randomWords: new uint256[](0),
+            exists: true,
+            fulfilled: false
+        });
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        emit RequestSent(requestId, numWords);
+        return requestId; // requestID is a uint.
+    }
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal override {
+        require(s_requests[_requestId].exists, "request not found");
+        s_requests[_requestId].fulfilled = true;
+        s_requests[_requestId].randomWords = _randomWords;
+
+        randomWordsNum = _randomWords[0]; // Set array-index to variable, easier to play with
+        emit RequestFulfilled(_requestId, _randomWords);
+    }
+
+    // to check the request status of random number call.
+    function getRequestStatus(
+        uint256 _requestId
+    ) external view returns (bool fulfilled, uint256[] memory randomWords) {
+        require(s_requests[_requestId].exists, "request not found");
+        RequestStatus memory request = s_requests[_requestId];
+        return (request.fulfilled, request.randomWords);
+    }
+
+    function withdrawAll() external onlyOwner {
+        require(address(this).balance > 0, "Contract has no bnb");
+        _wallet.transfer(address(this).balance);
+    }
+
+    function withdraw() external onlyOwner {
+        require(address(this).balance > 0, "Contract has no bnb");
+        _wallet.transfer(totalFee);
+        totalFee = 0;
+    }
+
+    function setWalletReceiver(address payable newWallet) external onlyOwner {
+        _wallet = newWallet;
+    }
+
+    function setSubscriptionId(uint64 newSubscriptionId) external onlyOwner {
+        s_subscriptionId = newSubscriptionId;
+    }
+}
---
+++ contracts/mainnet/d8/D85039EC81dD2B86fb8122d47096192344a37397_GensoVote.sol
@@ -0,0 +1,358 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.7.0) (access/Ownable.sol)
+
+pragma solidity ^0.8.0;
+
+import "../utils/Context.sol";
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
+    function _checkOwner() internal view virtual {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.7.0) (token/ERC1155/IERC1155.sol)
+
+pragma solidity ^0.8.0;
+
+import "../../utils/introspection/IERC165.sol";
+
+/**
+ * @dev Required interface of an ERC1155 compliant contract, as defined in the
+ * https://eips.ethereum.org/EIPS/eip-1155[EIP].
+ *
+ * _Available since v3.1._
+ */
+interface IERC1155 is IERC165 {
+    /**
+     * @dev Emitted when `value` tokens of token type `id` are transferred from `from` to `to` by `operator`.
+     */
+    event TransferSingle(address indexed operator, address indexed from, address indexed to, uint256 id, uint256 value);
+
+    /**
+     * @dev Equivalent to multiple {TransferSingle} events, where `operator`, `from` and `to` are the same for all
+     * transfers.
+     */
+    event TransferBatch(
+        address indexed operator,
+        address indexed from,
+        address indexed to,
+        uint256[] ids,
+        uint256[] values
+    );
+
+    /**
+     * @dev Emitted when `account` grants or revokes permission to `operator` to transfer their tokens, according to
+     * `approved`.
+     */
+    event ApprovalForAll(address indexed account, address indexed operator, bool approved);
+
+    /**
+     * @dev Emitted when the URI for token type `id` changes to `value`, if it is a non-programmatic URI.
+     *
+     * If an {URI} event was emitted for `id`, the standard
+     * https://eips.ethereum.org/EIPS/eip-1155#metadata-extensions[guarantees] that `value` will equal the value
+     * returned by {IERC1155MetadataURI-uri}.
+     */
+    event URI(string value, uint256 indexed id);
+
+    /**
+     * @dev Returns the amount of tokens of token type `id` owned by `account`.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     */
+    function balanceOf(address account, uint256 id) external view returns (uint256);
+
+    /**
+     * @dev xref:ROOT:erc1155.adoc#batch-operations[Batched] version of {balanceOf}.
+     *
+     * Requirements:
+     *
+     * - `accounts` and `ids` must have the same length.
+     */
+    function balanceOfBatch(address[] calldata accounts, uint256[] calldata ids)
+        external
+        view
+        returns (uint256[] memory);
+
+    /**
+     * @dev Grants or revokes permission to `operator` to transfer the caller's tokens, according to `approved`,
+     *
+     * Emits an {ApprovalForAll} event.
+     *
+     * Requirements:
+     *
+     * - `operator` cannot be the caller.
+     */
+    function setApprovalForAll(address operator, bool approved) external;
+
+    /**
+     * @dev Returns true if `operator` is approved to transfer ``account``'s tokens.
+     *
+     * See {setApprovalForAll}.
+     */
+    function isApprovedForAll(address account, address operator) external view returns (bool);
+
+    /**
+     * @dev Transfers `amount` tokens of token type `id` from `from` to `to`.
+     *
+     * Emits a {TransferSingle} event.
+     *
+     * Requirements:
+     *
+     * - `to` cannot be the zero address.
+     * - If the caller is not `from`, it must have been approved to spend ``from``'s tokens via {setApprovalForAll}.
+     * - `from` must have a balance of tokens of type `id` of at least `amount`.
+     * - If `to` refers to a smart contract, it must implement {IERC1155Receiver-onERC1155Received} and return the
+     * acceptance magic value.
+     */
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes calldata data
+    ) external;
+
+    /**
+     * @dev xref:ROOT:erc1155.adoc#batch-operations[Batched] version of {safeTransferFrom}.
+     *
+     * Emits a {TransferBatch} event.
+     *
+     * Requirements:
+     *
+     * - `ids` and `amounts` must have the same length.
+     * - If `to` refers to a smart contract, it must implement {IERC1155Receiver-onERC1155BatchReceived} and return the
+     * acceptance magic value.
+     */
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] calldata ids,
+        uint256[] calldata amounts,
+        bytes calldata data
+    ) external;
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (utils/Counters.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @title Counters
+ * @author Matt Condon (@shrugs)
+ * @dev Provides counters that can only be incremented, decremented or reset. This can be used e.g. to track the number
+ * of elements in a mapping, issuing ERC721 ids, or counting request ids.
+ *
+ * Include with `using Counters for Counters.Counter;`
+ */
+library Counters {
+    struct Counter {
+        // This variable should never be directly accessed by users of the library: interactions must be restricted to
+        // the library's function. As of Solidity v0.5.2, this cannot be enforced, though there is a proposal to add
+        // this feature: see https://github.com/ethereum/solidity/issues/4637
+        uint256 _value; // default: 0
+    }
+
+    function current(Counter storage counter) internal view returns (uint256) {
+        return counter._value;
+    }
+
+    function increment(Counter storage counter) internal {
+        unchecked {
+            counter._value += 1;
+        }
+    }
+
+    function decrement(Counter storage counter) internal {
+        uint256 value = counter._value;
+        require(value > 0, "Counter: decrement overflow");
+        unchecked {
+            counter._value = value - 1;
+        }
+    }
+
+    function reset(Counter storage counter) internal {
+        counter._value = 0;
+    }
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (utils/introspection/IERC165.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Interface of the ERC165 standard, as defined in the
+ * https://eips.ethereum.org/EIPS/eip-165[EIP].
+ *
+ * Implementers can declare support of contract interfaces, which can then be
+ * queried by others ({ERC165Checker}).
+ *
+ * For an implementation, see {ERC165}.
+ */
+interface IERC165 {
+    /**
+     * @dev Returns true if this contract implements the interface defined by
+     * `interfaceId`. See the corresponding
+     * https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified[EIP section]
+     * to learn more about how these ids are created.
+     *
+     * This function call must use less than 30 000 gas.
+     */
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GensoVote is Ownable {
+    using Counters for Counters.Counter;
+
+    address public to; // Address to receive the transferred ERC1155 tokens
+    address public asset_contract; // Address of the ERC1155 contract
+    uint256 public token_id; // ID of the ERC1155 token
+    Counters.Counter private voteCounter; // Counter to track the number of votes
+
+    event Vote(bytes32 encryptedData, address indexed voter, uint256 voteCount); // Event to record encrypted data, voter address, and vote count
+
+    constructor() {
+        token_id = 1200000000691; // Initial token ID
+        to = 0xF78904144f022aa94B5fE8dB8c3E0d0c8539c7B5; // Initial receiver address
+        asset_contract = 0xa72815200BA44a2472b24EbE22E453e49904eC33; // Initial ERC1155 contract address
+    }
+
+    // Setter function to update the receiver address
+    function setTo(address _to) public onlyOwner {
+        to = _to;
+    }
+
+    // Setter function to update the ERC1155 contract address
+    function setAssetContract(address _assetContract) public onlyOwner {
+        asset_contract = _assetContract;
+    }
+
+    // Setter function to update the token ID
+    function setTokenId(uint256 _tokenId) public onlyOwner {
+        token_id = _tokenId;
+    }
+
+    // Function to send ERC1155 tokens
+    function vote(bytes32 encryptedData) public {
+        IERC1155(asset_contract).safeTransferFrom(msg.sender, to, token_id, 1, "");
+
+        voteCounter.increment(); // Increment the vote count
+
+        emit Vote(encryptedData, msg.sender, voteCounter.current());
+    }
+
+    // Function to get the current vote count
+    function getVoteCount() public view returns (uint256) {
+        return voteCounter.current();
+    }
+}
