0e2134aaeff7ca04d5c3f656089e6185dc658aa3
minion <minion>
2023-06-29 17:11:21 +0000

    with 💕

---
+++ contracts/mainnet/40/40e7e7282e95f212c1d360f2d9Da0AB336D22011_DexWinTipsUtilsV2.sol
@@ -0,0 +1,191 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.8.0) (security/ReentrancyGuard.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Contract module that helps prevent reentrant calls to a function.
+ *
+ * Inheriting from `ReentrancyGuard` will make the {nonReentrant} modifier
+ * available, which can be applied to functions to make sure there are no nested
+ * (reentrant) calls to them.
+ *
+ * Note that because there is a single `nonReentrant` guard, functions marked as
+ * `nonReentrant` may not call one another. This can be worked around by making
+ * those functions `private`, and then adding `external` `nonReentrant` entry
+ * points to them.
+ *
+ * TIP: If you would like to learn more about reentrancy and alternative ways
+ * to protect against it, check out our blog post
+ * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
+ */
+abstract contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
+
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor() {
+        _status = _NOT_ENTERED;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and making it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        _nonReentrantBefore();
+        _;
+        _nonReentrantAfter();
+    }
+
+    function _nonReentrantBefore() private {
+        // On the first call to nonReentrant, _status will be _NOT_ENTERED
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+    }
+
+    function _nonReentrantAfter() private {
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
+    }
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./IDexwinCore.sol";
+
+contract DexWinTipsUtilsV2 is ReentrancyGuard {
+    address private s_owner;
+    bool private allowed = true;
+    IDexwinCore private immutable i_core;
+
+    constructor(address owner, address payable core) {
+        s_owner = owner;
+        i_core = IDexwinCore(core);
+    }
+
+    modifier onlyUtilsOwner() {
+        if (msg.sender != s_owner) {
+            revert("Utils__OnlyOwnerMethod");
+        }
+        _;
+    }
+
+    modifier isAllowed() {
+        if (!allowed) {
+            revert("Disabled");
+        }
+        _;
+    }
+
+    function transferUtilsOwnership(address newOwner) public onlyUtilsOwner {
+        _transferUtilsOwnership(newOwner);
+    }
+
+    function _transferUtilsOwnership(address newOwner) internal {
+        require(newOwner != address(0), "Incorrect address");
+        s_owner = newOwner;
+    }
+
+    function modifyAllow(bool status) public onlyUtilsOwner {
+        allowed = status;
+    }
+
+    function setCoreOwnershipInUtils(address newOwner) public onlyUtilsOwner {
+        i_core.setCoreOwnership(newOwner);
+    }
+
+    function disableCoreOwnershipInUtils(address owner) public onlyUtilsOwner {
+        i_core.disableCoreOwnership(owner);
+    }
+
+    function handleTips(address token, uint256 amount) public nonReentrant isAllowed {
+        uint256 tips = i_core.getUserTips(msg.sender, token);
+        uint256 bal = i_core.getUserBalance(msg.sender, token);
+        uint256 tipsToSubtract;
+        if (amount > tips + bal) revert("Utils__StakeMorethanbal");
+        if (tips > 0) {
+            tipsToSubtract = (tips >= amount) ? amount : tips;
+            i_core.handleUserTips(msg.sender, token, tipsToSubtract, 0);
+            i_core.handleBalance(msg.sender, token, tipsToSubtract, 1);
+        }
+    }
+
+    function getUtilsOwner() public view returns (address) {
+        return s_owner;
+    }
+}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+interface IDexwinCore {
+    function getBaseProvider(address token) external view returns (address);
+
+    function getRandomProvider(address token, uint256 randomWord) external returns (address);
+
+    function getUserBalance(address account, address token) external view returns (uint256);
+
+    function getTotalFunds(address token) external view returns (uint256);
+
+    function getUserTips(address account, address token) external view returns (uint256);
+
+    function getTotalUserTips(address token) external view returns (uint256);
+
+    function getUserStaked(address account, address token) external view returns (uint256);
+
+    function getTotalStakes(address token) external view returns (uint256);
+
+    function getDepositerHLBalance(address depositer, address token) external view returns (uint256);
+
+    function getTotalHL(address token) external view returns (uint256);
+
+    function getProviderPayout(address account, address token) external view returns (uint256);
+
+    function getTotalPayout(address token) external view returns (uint256);
+
+    function getBalancedStatus(address token) external view returns (bool);
+
+    function setCoreOwnership(address newOwner) external;
+
+    function disableCoreOwnership(address owwner) external;
+
+    function setTrustedForwarder(address trustedForwarder) external;
+
+    function addTokens(address token) external;
+
+    function disableToken(address token) external;
+
+    function setBaseProvider(address account, address token) external;
+
+    function handleBalance(address bettor, address token, uint256 amount, uint256 operator) external;
+
+    function handleUserTips(address bettor, address token, uint256 amount, uint256 operator) external;
+
+    function handleStakes(address bettor, address token, uint256 amount, uint256 operator) external;
+
+    function handleHL(address bettor, address token, uint256 amount, uint256 operator) external;
+
+    function handlePayout(address bettor, address token, uint256 amount, uint256 operator) external;
+}
